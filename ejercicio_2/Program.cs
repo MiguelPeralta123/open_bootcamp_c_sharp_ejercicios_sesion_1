61faaeb78d2fee8e7ca538405d85d0690c7792d1
Miguel Peralta <maikperalta123@gmail.com>
2023-04-03 18:42:24 -0700

    Ejercicio terminado

---
+++ ejercicio_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe la hora:");
+            string hora = Console.ReadLine();
+            Console.WriteLine("La hora es: " + hora);
+        }
+    }
+}
---
+++ ejercicios_sesion_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicios_sesion_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe tu nombre:");
+            string nombre = Console.ReadLine();
+            Console.WriteLine("Tu nombre es " + nombre);
+        }
+    }
+}
